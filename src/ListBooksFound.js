a1f60dca21a1bc1b05f54553801cc5e41e5af82c
RanaivosonHerimanitra <air_manitra@yahoo.fr>
2017-09-21 18:16:57 -0400

    cond rendering on book author

--- src/ListBooksFound.js
+++ src/ListBooksFound.js
@@ -30,7 +30,7 @@ class ListBooksFound extends Component {
                                        </div>
                                 </div>
                                       <div className="book-title">{book.title}</div>
-                                      <div className="book-authors">{book.authors[0]}</div>
+                                      <div className="book-authors">{book.authors ? book.authors[0] : ''}</div>
                             </div>
                         </li>  
                     
